50ea83aa5831fc6d7fd10a832fad196588b9c67d
rikharigaurav <141213738+rikharigaurav@users.noreply.github.com>
2024-05-04 23:50:43 +0530

    Deleted Files

--- app/board/[boardId]/_components/participants.tsx
+++
@@ -1,7 +0,0 @@
-export const Participants = () => {
-    return (
-        <div className="absolute h-12 top-2 right-2 bg-white rounded-md p-3 flex items-center shadow-md">
-            List of Users
-        </div>
-    )
-}
